e1e89ed9f85e7e60ca708632139a3a65531effc8
applicant-apply <95372497+applicant-apply@users.noreply.github.com>
2022-01-03 05:06:55 +0100

    Add files via upload

---
+++ message.js
@@ -0,0 +1,10 @@
+
+const load = document.getElementById('load');
+const msg= document.getElementById('msg');
+msg.style.display='none'
+  setTimeout(hideElement, 10000) //milliseconds until timeout//
+  function hideElement() {
+    load.style.display = 'none';
+    msg.style.display='inline'
+
+  }
